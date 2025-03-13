6837a7a2b6780b7c407d75e808dbdbd99e0ba957
samyak2003-coder <samyaksinghal2003@gmail.com>
2025-03-13 21:25:53 +0530

    order endpoint added:

---
+++ src/main/java/com/C2CApp/C2CBackend/services/OrderService.java
@@ -0,0 +1,38 @@
+package com.C2CApp.C2CBackend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.C2CApp.C2CBackend.repositories.OrderRepository;
+import org.springframework.stereotype.Service;
+import com.C2CApp.C2CBackend.entities.OrderSchema;
+import java.util.List;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository){
+        this.orderRepository = orderRepository;
+    }
+
+    public List<OrderSchema> getOrders() {
+        return orderRepository.findAll();
+    }
+
+    public List<OrderSchema> getOrderByBuyerId(String buyerId){
+        return orderRepository.findByBuyerId(buyerId);
+    }
+
+    public List<OrderSchema> getOrderBySellerId(String sellerId){
+        return orderRepository.findBySellerId(sellerId);
+    }
+
+    public void createOrder(OrderSchema order){
+        orderRepository.save(order);
+    }
+
+    public void deleteOrder(String orderId){
+        orderRepository.deleteById(orderId);
+    }
+}
